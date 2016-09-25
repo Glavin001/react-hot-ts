7ef9a27864841d6291e2646777f676b34dcd3b4f
Glavin Wiechert <glavin.wiechert@gmail.com>
2016-09-24 23:46:25 -0300

    Switch ts-loader to awesome-typescript-loader

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
     module: {
         loaders: [
             // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
-            { test: /\.tsx?$/, loader: "ts-loader" }
+            { test: /\.tsx?$/, loader: "awesome-typescript-loader" }
         ],
 
         preLoaders: [
