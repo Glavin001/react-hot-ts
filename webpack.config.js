02ab9011b5ee7f035dc7546f67ec7579a0b829fa
TruongSinh Tran-Nguyen <i@truongsinh.pro>
2016-11-28 18:44:03 +0200

    now, the state is retained!

--- webpack.config.js
+++ webpack.config.js
@@ -23,11 +23,13 @@ module.exports = {
     },
 
     plugins: [
+        /*
         new webpack.DefinePlugin({
             'process.env': {
                 'NODE_ENV': JSON.stringify('production')
             }
         }),
+        */
         new webpack.HotModuleReplacementPlugin(),
     ],
 
